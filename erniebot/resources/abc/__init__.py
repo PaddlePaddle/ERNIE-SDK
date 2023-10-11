bb350201f9fba2950a1ec1fa6c1f9c0865ac2c42
Bobholamovic <mhlin425@whu.edu.cn>
2023-10-11 21:02:12 +0800

    Add erniebot.FineTuningTask.cancel

--- erniebot/resources/abc/__init__.py
+++ erniebot/resources/abc/__init__.py
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from .cancellable import Cancellable
 from .creatable import Creatable
 from .queryable import Queryable
 
-__all__ = ['Creatable', 'Queryable']
+__all__ = ['Cancellable', 'Creatable', 'Queryable']
