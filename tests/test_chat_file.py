ec8727cb0c83613d1ed60c981b5d93e3172b4ec7
Bobholamovic <mhlin425@whu.edu.cn>
2023-10-25 14:21:34 +0800

    Fix chat_file test bug

--- tests/test_chat_file.py
+++ tests/test_chat_file.py
@@ -41,5 +41,5 @@ if __name__ == '__main__':
             'role': 'user',
             'content': "应对未来的技术挑战，AI框架有哪些独特的技术特色？"
         }],
-        stream=False)
+    )
     print(response.get_result())
