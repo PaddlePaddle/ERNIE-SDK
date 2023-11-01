f0189de01310a043e2c906fabde74e8a885b2a83
juncaipeng <13006307475@163.com>
2023-11-01 20:46:16 +0800

    [Feature][Agent] Add ERNIEBot model and setup to EB Agent

---
+++ erniebot-agent/README.md
@@ -0,0 +1,15 @@
+<div align="center">
+
+<h1>ERNIE Bot Agent</h1>
+
+ERNIE Bot Agent 可以快速开发智能体。
+
+[![License](https://img.shields.io/badge/license-Apache%202-blue.svg)](LICENSE)
+![python version](https://img.shields.io/badge/python-3.8+-orange.svg)
+![support os](https://img.shields.io/badge/os-linux%2C%20win%2C%20mac-yellow.svg)
+
+</div>
+
+## License
+
+ERNIE Bot Agent遵循Apache-2.0开源协议。
---
+++ erniebot-agent/erniebot_agent/message.py
@@ -0,0 +1,61 @@
+# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import List
+from erniebot.resources.chat_completion import ChatResponse
+
+class Message:
+    """The base class of message."""
+    def __init__(self, role, content):
+        self.role = role
+        self.content = content
+
+    def to_dict(self) -> list:
+        return {'role': self.role, 'content': self.content}
+    
+    def __str__(self) -> str:
+        return f'role:{self.role}, content: {self.content}'
+
+class HumanMessage(Message):
+    """A Message from human."""
+    
+    def __init__(self, content):
+        super().__init__(role='user', content=content)
+
+
+class AIMessage(Message):
+    """A Message from assistant."""
+    
+    def __init__(self, content):
+        super().__init__(role='assistant', content=content)
+
+class FunctionMessage(Message):
+    """A Message from assistant for function calling."""
+
+    def __init__(self, function_call):
+        super().__init__(role='assistant', content='null')
+        self.function_call = function_call
+
+    def to_dict(self) -> list:
+        return {'role': self.role, 'content': self.content, 'function_call': self.function_call}
+    
+    def __str__(self) -> str:
+        return f'role:{self.role}, content: {self.content}, function_call: {self.function_call}'
+
+def response_to_message(response: ChatResponse):
+    """Convert the response from assistant to AIMessage or FunctionMessage."""
+    if hasattr(response, 'function_call'):
+        return FunctionMessage(function_call=response.get_result())
+    else:
+        return AIMessage(content=response.get_result())
