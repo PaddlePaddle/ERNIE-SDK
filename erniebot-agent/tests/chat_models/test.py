d987aa4b12b3c744656060c5b0fa5ded5fd5e93e
juncaipeng <13006307475@163.com>
2023-11-01 20:53:45 +0800

    remove access token

--- erniebot-agent/tests/chat_models/test.py
+++ erniebot-agent/tests/chat_models/test.py
@@ -5,7 +5,7 @@ from erniebot_agent.message import HumanMessage, AIMessage
 
 async def test_ernie_bot(model='ernie-bot-turbo', stream=False):
     api_type = 'aistudio'
-    access_token = '1a76642806d7ccb26c8f6412c83450254ac9d680'
+    #access_token = 'your access token'
     eb = ERNIEBot(model=model, api_type=api_type, access_token=access_token)
     messages = [
         HumanMessage(content="我在深圳，周末可以去哪里玩？"),
