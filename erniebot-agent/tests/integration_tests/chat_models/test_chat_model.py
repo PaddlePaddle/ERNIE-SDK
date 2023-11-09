eb571f7938f4f9ba111a3b90093affe3d362462c
Sijun He <sijun.he@hotmail.com>
2023-11-09 10:58:19 +0800

    tests

--- erniebot-agent/tests/integration_tests/chat_models/test_chat_model.py
+++ erniebot-agent/tests/integration_tests/chat_models/test_chat_model.py
@@ -3,19 +3,19 @@ import unittest
 
 import pytest
 from erniebot_agent.chat_models.erniebot import ERNIEBot
-from erniebot_agent.message import FunctionMessage, HumanMessage
+from erniebot_agent.message import AIMessage, FunctionMessage, HumanMessage
 
 
-class TestChatModel(unittest.TestCase):
+class TestChatModel(unittest.IsolatedAsyncioTestCase):
     @pytest.mark.asyncio
     async def test_chat(self):
         eb = ERNIEBot(model="ernie-bot-turbo", api_type="aistudio", access_token=os.environ["access_token"])
         messages = [
-            HumanMessage(content="我在深圳，周末可以去哪里玩？"),
+            HumanMessage(content="你好！"),
         ]
         res = await eb.async_chat(messages, stream=False)
-        print(res)
-        self.assertTrue(False)
+        self.assertTrue(isinstance(res, AIMessage))
+        self.assertIsNotNone(res.content)
 
     @pytest.mark.asyncio
     async def test_function_call(self):
@@ -61,18 +61,21 @@ class TestChatModel(unittest.TestCase):
                 },
             }
         ]
-
+        # use ernie-bot here since ernie-bot-turbo doesn't support function call
         eb = ERNIEBot(model="ernie-bot", api_type="aistudio", access_token=os.environ["access_token"])
         messages = [
             HumanMessage(content="深圳市今天的气温是多少摄氏度？"),
         ]
         res = await eb.async_chat(messages, functions=functions)
-        print(res)
+        self.assertTrue(isinstance(res, AIMessage))
+        self.assertIsNone(res.content)
+        self.assertIsNotNone(res.function_call)
+        self.assertEqual(res.function_call["name"], "get_current_temperature")
 
         messages.append(res)
         messages.append(
             FunctionMessage(name="get_current_temperature", content='{"temperature":25,"unit":"摄氏度"}')
         )
         res = await eb.async_chat(messages, functions=functions)
-        print(res)
-        self.assertTrue(False)
+        self.assertTrue(isinstance(res, AIMessage))
+        self.assertIsNotNone(res.content)
