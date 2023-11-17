00172c8e7117a1683368e5b0fe6e5b9ec296922a
Southpika <122620817+Southpika@users.noreply.github.com>
2023-11-17 18:14:30 +0800

    [Logging]Add Logging Module (#70)
    * logging v0
    
    * logging v1
    
    * agent error

--- erniebot-agent/erniebot_agent/__init__.py
+++ erniebot-agent/erniebot_agent/__init__.py
@@ -11,3 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import logging
+
+from .utils.logging import setup_logging
+
+logger = logging.getLogger(__name__)
+setup_logging(logger)
