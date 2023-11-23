a29be2df825dd0803a36632814865a0192cbe72b
w5688414 <w5688414@gmail.com>
2023-11-23 16:59:50 +0800

    Add aurora search

--- erniebot-agent/erniebot_agent/tools/aurora_tool.py
+++ erniebot-agent/erniebot_agent/tools/aurora_tool.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import Any, Dict, List, Optional, Type
 
-from erniebot_agent.messages import AIMessage, HumanMessage, Message
+from erniebot_agent.messages import AIMessage, HumanMessage
 from erniebot_agent.tools.schema import ToolParameterView
 from pydantic import Field, create_model
 from pydantic.fields import FieldInfo
@@ -49,7 +49,7 @@ class AuroraSearchTool(Tool):
     input_type: Type[ToolParameterView] = AuroraSearchToolInputView
     ouptut_type: Type[ToolParameterView] = AuroraSearchToolOutputView
 
-    def __init__(self, description, db, input_type=None, output_type=None) -> None:
+    def __init__(self, description, db, input_type=None, output_type=None, examples=None) -> None:
         super().__init__()
         self.db = db
         self.description = description
@@ -57,22 +57,29 @@ class AuroraSearchTool(Tool):
             self.input_type = input_type
         if output_type is not None:
             self.ouptut_type = output_type
+        if examples is not None:
+            self.few_shot_examples = examples
 
     async def __call__(self, query: str, top_k: int = 10, filters: Optional[dict[str, Any]] = None):
         res = self.db.search(query, top_k, filters)
         return res
 
     @property
-    def examples(self) -> List[Message]:
-        return [
-            HumanMessage("城乡建设部规章中描述的城市管理执法的执法主体是谁？"),
-            AIMessage(
-                "",
-                function_call={
-                    "name": self.tool_name,
-                    "thoughts": f"这是一个住房和城乡建设部规章的问题，我们使用{self.tool_name}工具检索相关的信息，检索的query：'城市管理执法的执法主体'。",
-                    "arguments": '{"query": "城市管理执法的执法主体", "top_k": 3}',
-                },
-            ),
-        ]
-        # return []
+    def examples(
+        self,
+    ) -> List[Any]:
+        few_shot_objects: List[Any] = []
+        for item in self.few_shot_examples:
+            few_shot_objects.append(HumanMessage(item["user"]))
+            few_shot_objects.append(
+                AIMessage(
+                    "",
+                    function_call={
+                        "name": self.tool_name,
+                        "thoughts": item["thoughts"],
+                        "arguments": item["arguments"],
+                    },
+                )
+            )
+
+        return few_shot_objects
