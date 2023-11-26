ea9822042c79c1b3f68f9a45b357133f6664a9ff
shiyutang <34859558+shiyutang@users.noreply.github.com>
2023-11-27 00:05:37 +0800

    [Feathre]Add system message and token_count fallback (#88)
    * fix_mockbot
    
    * add_system_message
    
    * update

--- erniebot-agent/tests/unit_tests/memory/test_limit_token_memory.py
+++ erniebot-agent/tests/unit_tests/memory/test_limit_token_memory.py
@@ -2,7 +2,7 @@ import unittest
 
 import pytest
 from erniebot_agent.memory import LimitTokensMemory
-from erniebot_agent.messages import HumanMessage
+from erniebot_agent.messages import HumanMessage, SystemMessage
 
 from tests.unit_tests.testing_utils import MockErnieBot
 
@@ -26,7 +26,7 @@ class Testlimit_tokenMemory(unittest.IsolatedAsyncioTestCase):
     @pytest.mark.asyncio
     async def test_limit_token_memory_truncate_tokens(self, k=3):  # truncate through returned message
         # The memory
-        memory = LimitTokensMemory(4)
+        memory = LimitTokensMemory(20)
 
         for _ in range(k):
             # 2 times of human message
@@ -36,7 +36,30 @@ class Testlimit_tokenMemory(unittest.IsolatedAsyncioTestCase):
             message = await self.llm.async_chat(memory.get_messages())
             memory.add_message(message)
 
-        self.assertTrue(memory.mem_token_count <= 4)
+        self.assertTrue(memory.mem_token_count <= 20)
+
+    @pytest.mark.asyncio
+    async def test_limit_token_memory_truncate_tokens_system_message(
+        self, k=5
+    ):  # truncate through returned message
+        # The memory
+        memory = LimitTokensMemory(100)
+
+        # Keypoint 1: system message 的存取
+        memory.system_message = SystemMessage("你是一个善于回答图像相关问题的agent。")
+        self.assertTrue(memory.system_message.content == "你是一个善于回答图像相关问题的agent。")
+
+        for _ in range(k):
+            # 2 times of human message
+            memory.add_message(HumanMessage(content="这个图像中的内容是什么？"))
+
+            # AI message
+            message = await self.llm.async_chat(memory.get_messages())
+            memory.add_message(message)
+
+        # Keypoint 2:没有传入token_count 的fallback情况，此时也能正确裁剪信息
+        self.assertTrue(memory.mem_token_count <= 100)
+        self.assertTrue(len(memory.get_messages()) < 2 * k)
 
 
 if __name__ == "__main__":
