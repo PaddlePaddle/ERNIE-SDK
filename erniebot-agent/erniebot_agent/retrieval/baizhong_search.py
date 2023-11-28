9254b4196e0c604e3e05a783c73ff1d7e6f1c271
w5688414 <w5688414@gmail.com>
2023-11-28 10:59:27 +0800

    update comments for baizhong search

--- erniebot-agent/erniebot_agent/retrieval/baizhong_search.py
+++ erniebot-agent/erniebot_agent/retrieval/baizhong_search.py
@@ -33,6 +33,15 @@ class BaizhongSearch:
             self.projectId = projectId
 
     def create_project(self):
+        """
+        Create a project using the Baizhong API.
+
+        Returns:
+            dict: A dictionary containing information about the created project.
+
+        Raises:
+            BaizhongError: If the API request fails, this exception is raised with details about the error.
+        """
         json_data = {
             "projectName": self.projectName,
             "remark": self.remark,
@@ -47,6 +56,15 @@ class BaizhongSearch:
             raise BaizhongError(message=f"request error: {res.text}", error_code=res.status_code)
 
     def create_schema(self):
+        """
+        Create a schema for a project using the Baizhong API.
+
+        Returns:
+            dict: A dictionary containing information about the created schema.
+
+        Raises:
+            BaizhongError: If the API request fails, this exception is raised with details about the error.
+        """
         json_data = {
             "projectId": self.projectId,
             "schemaJson": {
@@ -71,6 +89,15 @@ class BaizhongSearch:
     def update_schema(
         self,
     ):
+        """
+        Update the schema for a project using the Baizhong API.
+
+        Returns:
+            dict: A dictionary containing information about the updated schema.
+
+        Raises:
+            BaizhongError: If the API request fails, this exception is raised with details about the error.
+        """
         json_data = {
             "projectId": self.projectId,
             "schemaJson": {
@@ -94,6 +121,21 @@ class BaizhongSearch:
             raise BaizhongError(message=f"request error: {res.text}", error_code=res.status_code)
 
     def search(self, query: str, top_k: int = 10, filters: Optional[Dict[str, Any]] = None):
+        """
+        Perform a search using the Baizhong common search API.
+
+        Args:
+            query (str): The search query.
+            top_k (int, optional): The number of top results to retrieve (default is 10).
+            filters (Optional[Dict[str, Any]], optional): Additional filters to apply to the search query
+            (default is None).
+
+        Returns:
+            List[Dict[str, Any]]: A list of dictionaries containing search results.
+
+        Raises:
+            BaizhongError: If the API request fails, this exception is raised with details about the error.
+        """
         json_data = {
             "query": query,
             "projectId": self.projectId,
@@ -118,6 +160,23 @@ class BaizhongSearch:
             raise BaizhongError(message=f"request error: {res.text}", error_code=res.status_code)
 
     def add_documents(self, documents: List[Document], batch_size: int = 10, thread_count: int = 1):
+        """
+        Add a batch of documents to the Baizhong system using multi-threading.
+
+        Args:
+            documents (List[Document]): A list of Document objects to be added.
+            batch_size (int, optional): The size of each batch of documents (default is 10).
+            thread_count (int, optional): The number of threads to use for concurrent document addition
+            (default is 1).
+
+        Returns:
+            List[Union[None, Exception]]: A list of results from the document addition process.
+
+        Note:
+            This function uses multi-threading to improve the efficiency of adding a large number of
+            documents.
+
+        """
         if type(documents[0]) == Document:
             list_dicts = [item.to_dict() for item in documents]
         all_data = []
@@ -129,6 +188,18 @@ class BaizhongSearch:
         return res
 
     def get_document_by_id(self, doc_id):
+        """
+        Retrieve a document from the Baizhong system by its ID.
+
+        Args:
+            doc_id: The ID of the document to retrieve.
+
+        Returns:
+            dict: A dictionary containing information about the retrieved document.
+
+        Raises:
+            BaizhongError: If the API request fails, this exception is raised with details about the error.
+        """
         json_data = {"projectId": self.projectId, "followIndexFlag": True, "dataBody": [doc_id]}
         res = requests.post(f"{self.baseUrl}/baizhong/data-api/v2/flush/get", json=json_data)
         if res.status_code == 200:
@@ -143,6 +214,21 @@ class BaizhongSearch:
         self,
         ids: Optional[List[str]] = None,
     ):
+        """
+        Delete documents from the Baizhong system.
+
+        Args:
+            ids (Optional[List[str]], optional): A list of document IDs to delete. If not provided,
+            all documents will be deleted.
+
+        Returns:
+            dict: A dictionary containing information about the deletion process.
+
+        Raises:
+            NotImplementedError: If the deletion of all documents is attempted, this exception is raised
+            as it is not yet implemented.
+            BaizhongError: If the API request fails, this exception is raised with details about the error.
+        """
         json_data = {"projectId": self.projectId, "followIndexFlag": True}
         if ids:
             json_data["dataBody"] = ids
@@ -159,6 +245,18 @@ class BaizhongSearch:
             raise BaizhongError(message=f"request error: {res.text}", error_code=res.status_code)
 
     def _add_documents(self, documents: List[Dict[str, Any]]):
+        """
+        Internal method to add a batch of documents to the Baizhong system.
+
+        Args:
+            documents (List[Dict[str, Any]]): A list of dictionaries representing documents to be added.
+
+        Returns:
+            dict: A dictionary containing information about the document addition process.
+
+        Raises:
+            BaizhongError: If the API request fails, this exception is raised with details about the error.
+        """
         json_data = {"projectId": self.projectId, "followIndexFlag": True, "dataBody": documents}
         res = requests.post(f"{self.baseUrl}/baizhong/data-api/v2/flush/add", json=json_data)
         if res.status_code == 200:
@@ -172,6 +270,18 @@ class BaizhongSearch:
 
     @classmethod
     def delete_project(cls, project_id: int):
+        """
+        Class method to delete a project using the Baizhong API.
+
+        Args:
+            project_id (int): The ID of the project to be deleted.
+
+        Returns:
+            dict: A dictionary containing information about the deletion process.
+
+        Raises:
+            BaizhongError: If the API request fails, this exception is raised with details about the error.
+        """
         json_data = {"projectId": project_id}
         res = requests.post(f"{cls.baseUrl}/baizhong/web-api/v2/project/delete", json=json_data)
         if res.status_code == 200:
