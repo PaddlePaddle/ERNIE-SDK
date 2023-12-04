f0c56697c9ce2e7f7f4e1f27ce239095db50ab53
Bobholamovic <mhlin425@whu.edu.cn>
2023-12-04 14:10:52 +0800

    Remove FakeFunctionCallingChatModel

--- erniebot-agent/tests/unit_tests/testing_utils/mocks/mock_chat_models.py
+++ erniebot-agent/tests/unit_tests/testing_utils/mocks/mock_chat_models.py
@@ -1,12 +1,5 @@
-import itertools
-
 from erniebot_agent.chat_models.base import ChatModel
-from erniebot_agent.messages import (
-    AIMessage,
-    FunctionCall,
-    FunctionMessage,
-    HumanMessage,
-)
+from erniebot_agent.messages import AIMessage
 
 
 class FakeSimpleChatModel(ChatModel):
@@ -21,34 +14,6 @@ class FakeSimpleChatModel(ChatModel):
         if stream:
             raise ValueError("Streaming is not supported.")
         return self.response
-
-
-class FakeFunctionCallingChatModel(ChatModel):
-    def __init__(self):
-        super().__init__("function_calling_chat_model")
-
-    @property
-    def text_response(self):
-        return AIMessage(content="Text response", function_call=None, token_usage=None)
-
-    async def async_chat(self, messages, *, stream=False, functions=None, **kwargs):
-        if stream:
-            raise ValueError("Streaming is not supported.")
-        if functions is None:
-            raise TypeError("`functions` must be provided.")
-        last_message = messages[-1]
-        if isinstance(last_message, HumanMessage):
-            fake_function_call = self._create_fake_function_call(functions)
-            return AIMessage(content="", function_call=fake_function_call)
-        elif isinstance(last_message, FunctionMessage):
-            return self.text_response
-
-    def _create_fake_function_call(self, functions):
-        function = functions[0]
-        parameters = function["parameters"]
-        func_name = function["name"]
-        func_args = dict(zip(parameters["properties"].keys(), itertools.cycle([None])))
-        return FunctionCall(name=func_name, thoughts="Function call response", arguments=func_args)
 
 
 class FakeChatModelWithPresetResponses(ChatModel):
