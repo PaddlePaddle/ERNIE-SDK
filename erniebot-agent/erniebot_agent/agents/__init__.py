c8c9142577baefa2f4bd466680e2cb64a7bd82a4
Sijun He <sijun.he@hotmail.com>
2023-12-06 17:51:57 +0800

    [feat] Refine FunctionAgentWithRetrieval (#129)
    * fix
    
    * changes
    
    * change prompt

--- erniebot-agent/erniebot_agent/agents/__init__.py
+++ erniebot-agent/erniebot_agent/agents/__init__.py
@@ -11,3 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from .base import Agent
+from .functional_agent import FunctionalAgent
+from .functional_agent_with_retrieval import FunctionalAgentWithRetrieval
