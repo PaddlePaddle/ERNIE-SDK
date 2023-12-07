898ed17ab0db5ae07090be08c54b1860fcf637bf
wj-Mcat <1435130236@qq.com>
2023-12-07 19:20:42 +0800

    update file mimetype

--- erniebot-agent/erniebot_agent/tools/schema.py
+++ erniebot-agent/erniebot_agent/tools/schema.py
@@ -238,7 +238,7 @@ class ToolParameterView(BaseModel):
             if format is not None:
                 json_schema_extra["format"] = format
 
-            json_schema_extra["x-ebagent-file-type"] = field_dict.get("x-ebagent-file-type", "auto")
+            json_schema_extra["x-ebagent-file-mimetype"] = field_dict.get("x-ebagent-file-mimetype", "auto")
 
             field_info_param = dict(
                 annotation=field_type, description=description, json_schema_extra=json_schema_extra
