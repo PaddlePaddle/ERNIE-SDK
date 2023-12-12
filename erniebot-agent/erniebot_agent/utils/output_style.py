bd5595cb715ea1bbf717e7456ad0d90bcacff4ac
Southpika <513923576@qq.com>
2023-12-12 16:43:34 +0800

    revise the logic of color text

--- erniebot-agent/erniebot_agent/utils/output_style.py
+++ erniebot-agent/erniebot_agent/utils/output_style.py
@@ -12,13 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import annotations
+from typing import List, Optional, Union
 
-from typing import TYPE_CHECKING, List, Optional, Union
-
-if TYPE_CHECKING:
-    from erniebot_agent.messages import Message
-
+from erniebot_agent.messages import Message
 from erniebot_agent.utils.json import to_pretty_json
 
 __all__ = ["ColoredText"]
@@ -38,9 +34,15 @@ class ColoredText:
     role_color: dict
     max_length: int
 
-    def __init__(self, text: Union[str, Message, List[Message]], role: Optional[str] = None):
+    def __init__(
+        self,
+        text: Union[str, Message, List[Message]],
+        role: Optional[str] = None,
+        color: Optional[str] = None,
+    ):
         self.text = text
         self.role = role
+        self.color = color
 
     def __str__(self):
         return str(self.text)
@@ -55,11 +57,18 @@ class ColoredText:
 
     def get_colored_text(self):
         if isinstance(self.text, str):
-            return self.colorize_text(self.text, self.role_color[self.role])
+            if self.color:
+                # if user chose the color, use the color
+                return self._colorize_text(self.text, self.color)
+            elif self.role:
+                # if user chose the color, use the role color
+                return self._colorize_text(self.text, self.role_color[self.role])
+            else:
+                raise RuntimeError("Please specify the color or role of the text")
         else:
-            return self.colorize_msg(self.text, self.role_color)
+            return self._colorize_msg(self.text, self.role_color)
 
-    def colorize_text(self, text: str, color: Optional[str]) -> str:
+    def _colorize_text(self, text: str, color: Optional[str]) -> str:
         if color is not None and color not in _COLORS:
             color_keys = list(_COLORS.keys())
             raise ValueError("Only support colors: " + ", ".join(str(key) for key in color_keys))
@@ -69,7 +78,7 @@ class ColoredText:
         else:
             return _COLORS[color] + str(text) + _COLORS["RESET"]
 
-    def colorize_msg(self, message: Union[Message, List[Message]], role_color: dict) -> str:
+    def _colorize_msg(self, message: Union[Message, List[Message]], role_color: dict) -> str:
         max_length = self.max_length if self.max_length else 150
         res = ""
         if isinstance(message, list):
