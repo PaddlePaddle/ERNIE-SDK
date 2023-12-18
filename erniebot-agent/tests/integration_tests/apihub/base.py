94a33c8e5cf34315a77d0e9ef50a0335d79115a1
骑马小猫 <1435130236@qq.com>
2023-12-18 16:06:36 +0800

    [Testing]add integration test for remote-tool (#166)
    * add integration test
    
    * update integration test

---
+++ erniebot-agent/tests/integration_tests/apihub/base.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+import os
+import tempfile
+import unittest
+from typing import Optional
+
+import requests
+from erniebot_agent.agents.functional_agent import FunctionalAgent
+from erniebot_agent.chat_models import ERNIEBot
+from erniebot_agent.memory import WholeMemory
+from erniebot_agent.tools.base import RemoteToolkit
+from erniebot_agent.tools.tool_manager import ToolManager
+
+
+class RemoteToolTesting(unittest.IsolatedAsyncioTestCase):
+    def setUp(self) -> None:
+        self.temp_dir = tempfile.mkdtemp()
+
+    def download_file(self, url, file_name: Optional[str] = None):
+        image_response = requests.get(url)
+        if file_name is None:
+            file_name = os.path.basename(url)
+
+        path = os.path.join(self.temp_dir, file_name)
+        with open(path, "wb") as f:
+            f.write(image_response.content)
+
+        return path
+
+    def get_agent(self, toolkit: RemoteToolkit):
+        if "EB_BASE_URL" in os.environ:
+            llm = ERNIEBot(model="ernie-bot", api_type="custom")
+        else:
+            llm = ERNIEBot(model="ernie-bot")
+
+        return FunctionalAgent(
+            llm=llm,
+            tools=ToolManager(tools=toolkit.get_tools()),
+            memory=WholeMemory(),
+        )
---
+++ erniebot-agent/tests/integration_tests/apihub/test_doc_rm_bnd.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+import pytest
+from erniebot_agent.file_io.file_manager import FileManager
+from erniebot_agent.tools.base import RemoteToolkit
+
+from .base import RemoteToolTesting
+
+
+class TestRemoteTool(RemoteToolTesting):
+    @pytest.mark.asyncio
+    async def test_doc_rm_bnd(self):
+        toolkit = RemoteToolkit.from_aistudio("rm-doc-img-bnd")
+
+        file_manager = FileManager()
+
+        file = await file_manager.create_file_from_path(
+            self.download_file("https://paddlenlp.bj.bcebos.com/ebagent/ci/fixtures/remote-tools/biyadi.png")
+        )
+        agent = self.get_agent(toolkit)
+
+        result = await agent.async_run("请帮我把这张图片的底纹给删掉", files=[file])
+        self.assertEqual(len(result.files), 2)
