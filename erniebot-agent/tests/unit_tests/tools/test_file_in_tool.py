f0232ac1d86df44e0191def8b456b5396688863a
Sijun He <sijun.he@hotmail.com>
2023-12-19 13:53:08 +0800

    fix test

--- erniebot-agent/tests/unit_tests/tools/test_file_in_tool.py
+++ erniebot-agent/tests/unit_tests/tools/test_file_in_tool.py
@@ -26,7 +26,7 @@ import uuid
 import uvicorn
 from erniebot_agent.file_io.file_manager import FileManager
 from erniebot_agent.tools import RemoteToolkit
-from erniebot_agent.tools.base import (
+from erniebot_agent.tools.utils import (
     get_file_info_from_param_view,
     parse_file_from_json_response,
 )
