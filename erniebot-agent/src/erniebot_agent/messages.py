62f9dbfa7625f051d53a170a716ae37bc1f7be85
tangshiyu <tangshiyu@baidu.com>
2023-12-22 10:30:01 +0800

    v1

--- erniebot-agent/src/erniebot_agent/messages.py
+++ erniebot-agent/src/erniebot_agent/messages.py
@@ -24,8 +24,24 @@ logger = logging.getLogger(__name__)
 
 
 class Message:
-    """The base class of a message."""
+    """
+    Base class of the message.
 
+    Args:
+        role (str): character of the message.
+        content (str): content of the message.
+        token_count (Optional[int], optional): number of tokens of the message content. Defaults to None.
+
+    Returns:
+        A message object。
+    
+    Examples:
+        >>> Message("user", "hello")
+        <role: user, content: hello>
+        >>> Message("user", "hello", token_count=5)
+        <role: user, content: hello, token_count: 5>
+
+    """
     def __init__(self, role: str, content: str, token_count: Optional[int] = None):
         self._role = role
         self._content = content
@@ -42,19 +58,20 @@ class Message:
 
     @property
     def token_count(self):
-        """Get the number of tokens of the message."""
         if self._token_count is None:
             raise AttributeError("The token count of the message has not been set.")
         return self._token_count
 
     @token_count.setter
     def token_count(self, token_count: int):
-        """Set the number of tokens of the message."""
         if self._token_count is not None:
             raise AttributeError("The token count of the message can only be set once.")
         self._token_count = token_count
 
     def to_dict(self) -> Dict[str, str]:
+        """
+        Transfer the message to a dict, key is the params.
+        """
         res = {}
         for name in self._param_names:
             res[name] = getattr(self, name)
@@ -78,14 +95,59 @@ class Message:
 
 
 class SystemMessage(Message):
-    """A message from a human to set system information."""
+    r"""
+    Definition of system message, such that the feature of the Agent can be customized.
 
+    Args:
+        content (str): the content of the message.
+    
+    Returns:
+        A system message object.
+    
+     Examples:
+
+        .. code-block:: python
+        >>> from erniebot_agent.messages import SystemMessage
+        >>> SystemMessage("you are an assistant useful for ocr.")
+        <role: system, content: you are an assistant useful for ocr.>
+        >>> SystemMessage("you are an assistant useful for ocr.").to_dict()
+        {'role': 'system', 'content': 'you are an assistant useful for ocr.'}
+        >>> SystemMessage("you are an assistant useful for ocr.").token_count
+        2
+        >>> SystemMessage("you are an assistant useful for ocr.").token_count = 3
+        >>> SystemMessage("you are an assistant useful for ocr.").token_count
+        3
+
+    """
     def __init__(self, content: str):
         super().__init__(role="system", content=content, token_count=len(content))
 
 
 class HumanMessage(Message):
-    """A message from a human."""
+    """
+    The definition of the message created by a human.
+    
+    Args:
+        content (str): the content of the message.
+
+    Returns:
+        A HumanMessage object.
+    
+    Examples:
+    .. code-block:: python
+    >>> from erniebot_agent.messages import HumanMessage
+    >>> HumanMessage("I want to order a pizza.")
+    <role: user, content: I want to order a pizza.>
+
+    >>> from erniebot_agent.file_io.base import File
+    >>> prompt = "I want to know text in this image."
+    >>> files = [await file_manager.create_file_from_path(file_path="ocr_img.jpg", file_type="remote")]
+    >>> message = await HumanMessage.create_with_files(
+            prompt, files, include_file_urls=True)
+    >>> message
+    <role: user, content: I want to know text in this image.<file>File-local-xxxx</file><url>{url}</url>.>
+        
+    """
 
     def __init__(self, content: str):
         super().__init__(role="user", content=content)
@@ -94,6 +156,20 @@ class HumanMessage(Message):
     async def create_with_files(
         cls, text: str, files: List[File], *, include_file_urls: bool = False
     ) -> Self:
+        """
+        create a Human Message with file input
+        
+        Args:
+            text: content of the message.
+            files (List[File]): The file that the message contains. 
+            include_file_urls: Whehter to include file URLs in the content of message.
+        
+        Returns:
+            A HumanMessage object contains file.
+        
+        Raises:
+            RuntimeError: Only `RemoteFile` objects can set include_file_urls as True.
+        """
         def _get_file_reprs(files: List[File]) -> List[str]:
             file_reprs = []
             for file in files:
@@ -144,7 +220,31 @@ class TokenUsage(TypedDict):
 
 
 class AIMessage(Message):
-    """A message from an assistant."""
+    """
+    The definition of the message from an assistant.
+    Args:
+        content (str): the content of the message.
+        function_call (Optional[FunctionCall], optional): The function that agent calls. Defaults to None.
+        token_usage (Optional[TokenUsage], optional): the token usage calculate by ERNIE. Defaults to None.
+        search_info (Optional[SearchInfo], optional): The SearchInfo content of the chat model's response. Defaults to None.
+    
+    Examples:
+        
+        .. code-block:: python
+
+            >>> human_message = HumanMessage(content="I want to know text in this image.")
+            >>> ai_message = AIMessage(
+                function_call={"name": "OCR", "thoughts": "The user want to know the text in the image, \
+                     I need to use the OCR tool", "arguments": "{\"imgae_byte_str\": file-remote-xxxx, \"lang\": "en"}"},
+                token_usage={"prompt_tokens": 10, "completion_tokens": 20},
+                search_info={}]}
+                )
+            >>> human_message.content
+            "I want to know text in this image."
+            >>> ai_message.function_call
+            {"name": "OCR", "thoughts": "The user want to know the text in the image, I need to use the OCR tool",\
+                     "arguments": "{\"imgae_byte_str\": file-remote-xxxx, \"lang\": "en"}"}
+    """
 
     def __init__(
         self,
@@ -170,7 +270,8 @@ class AIMessage(Message):
 
 
 class FunctionMessage(Message):
-    """A message from a human, containing the result of a function call."""
+    """
+    The definition of a message that calls tools, containing the result of a function call."""
 
     def __init__(self, name: str, content: str):
         super().__init__(role="function", content=content)
