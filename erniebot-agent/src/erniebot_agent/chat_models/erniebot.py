c3fe96d0bf4cd473fe9a8f3cb2e659b60ef95f57
Southpika <513923576@qq.com>
2023-12-25 19:10:58 +0800

    pass fomat

--- erniebot-agent/src/erniebot_agent/chat_models/erniebot.py
+++ erniebot-agent/src/erniebot_agent/chat_models/erniebot.py
@@ -78,7 +78,7 @@ class ERNIEBot(ChatModel):
                 if "ak" and "sk" not in self.default_chat_kwargs:
                     ak, sk = C.get_global_aksk()
                     if not ak:
-                        raise RuntimeError("Please set at least one of ak+sk or AccessToken.")
+                        raise RuntimeError("Please set at least one of ak+sk or accesstoken.")
                     else:
                         self.ak = ak
                         self.sk = sk
