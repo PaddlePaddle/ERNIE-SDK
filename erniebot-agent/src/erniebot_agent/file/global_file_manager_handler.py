3ab12f74a836fd453ce0cfbd6c4ecbfa471f0cc7
Bobholamovic <mhlin425@whu.edu.cn>
2023-12-25 20:28:26 +0800

    Fix singleton

--- erniebot-agent/src/erniebot_agent/file/global_file_manager_handler.py
+++ erniebot-agent/src/erniebot_agent/file/global_file_manager_handler.py
@@ -20,11 +20,11 @@ import asyncio_atexit  # type: ignore
 from erniebot_agent.file.file_manager import FileManager
 from erniebot_agent.file.remote_file import AIStudioFileClient
 from erniebot_agent.utils import config_from_environ as C
-from erniebot_agent.utils.misc import Singleton
+from erniebot_agent.utils.misc import SingletonMeta
 
 
 @final
-class GlobalFileManagerHandler(Singleton):
+class GlobalFileManagerHandler(metaclass=SingletonMeta):
     _file_manager: Optional[FileManager]
 
     def __init__(self) -> None:
@@ -40,7 +40,7 @@ class GlobalFileManagerHandler(Singleton):
                     save_dir=None,
                     enable_remote_file=False,
                 )
-        return self._file_manager
+            return self._file_manager
 
     async def configure(
         self,
