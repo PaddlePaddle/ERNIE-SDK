ee37764d890c840982e32f8b4c3b1343eabf151c
w5688414 <w5688414@gmail.com>
2023-12-25 09:44:45 +0000

    remove unitest functions

--- erniebot-agent/tests/unit_tests/agents/test_functional_agent_with_retrieval_tool.py
+++ erniebot-agent/tests/unit_tests/agents/test_functional_agent_with_retrieval_tool.py
@@ -12,7 +12,7 @@ from tests.unit_tests.testing_utils.mocks.mock_chat_models import (
 from tests.unit_tests.testing_utils.mocks.mock_memory import FakeMemory
 from tests.unit_tests.testing_utils.mocks.mock_tool import FakeTool
 
-from erniebot_agent.agents import FunctionalAgentWithRetrieval
+from erniebot_agent.agents import FunctionalAgentWithRetrievalTool
 from erniebot_agent.memory import AIMessage, HumanMessage
 from erniebot_agent.memory.messages import FunctionCall
 from erniebot_agent.retrieval import BaizhongSearch
@@ -57,18 +57,17 @@ def no_input_no_output_tool():
 
 
 @pytest.mark.asyncio
-async def test_functional_agent_with_retrieval_callbacks(identity_tool):
+async def test_functional_agent_with_retrieval_tool_tool_callbacks(identity_tool):
     callback_handler = CountingCallbackHandler()
     knowledge_base_name = "test"
     access_token = "your access token"
     knowledge_base_id = 111
-    with mock.patch("requests.post") as my_mock:
-        search_db = BaizhongSearch(
-            knowledge_base_name=knowledge_base_name,
-            access_token=access_token,
-            knowledge_base_id=knowledge_base_id if knowledge_base_id != "" else None,
-        )
-    agent = FunctionalAgentWithRetrieval(
+    search_db = BaizhongSearch(
+        knowledge_base_name=knowledge_base_name,
+        access_token=access_token,
+        knowledge_base_id=knowledge_base_id if knowledge_base_id != "" else None,
+    )
+    agent = FunctionalAgentWithRetrievalTool(
         knowledge_base=search_db,
         llm=FakeSimpleChatModel(),
         tools=[identity_tool],
@@ -93,18 +92,24 @@ async def test_functional_agent_with_retrieval_callbacks(identity_tool):
 
 
 @pytest.mark.asyncio
-async def test_functional_agent_with_retrieval_run_retrieval(identity_tool):
+async def test_functional_agent_with_retrieval_tool_run_retrieval_tool(identity_tool):
     knowledge_base_name = "test"
     access_token = "your access token"
     knowledge_base_id = 111
-    with mock.patch("requests.post") as my_mock:
-        search_db = BaizhongSearch(
-            knowledge_base_name=knowledge_base_name,
-            access_token=access_token,
-            knowledge_base_id=knowledge_base_id if knowledge_base_id != "" else None,
-        )
-    agent = FunctionalAgentWithRetrieval(
-        knowledge_base=search_db, llm=FakeSimpleChatModel(), tools=[identity_tool], memory=FakeMemory()
+
+    search_db = BaizhongSearch(
+        knowledge_base_name=knowledge_base_name,
+        access_token=access_token,
+        knowledge_base_id=knowledge_base_id if knowledge_base_id != "" else None,
+    )
+    llm = FakeChatModelWithPresetResponses(
+        responses=[
+            AIMessage('{"is_relevant":true}', function_call=None),
+            AIMessage("Text response", function_call=None),
+        ]
+    )
+    agent = FunctionalAgentWithRetrievalTool(
+        knowledge_base=search_db, llm=llm, tools=[identity_tool], memory=FakeMemory()
     )
 
     # Test retrieval success
@@ -114,34 +119,48 @@ async def test_functional_agent_with_retrieval_run_retrieval(identity_tool):
 
     assert response.text == "Text response"
     # HumanMessage
+    assert response.chat_history[0].content == "Hello, world!"
+    # AIMessage
+    assert response.chat_history[1].function_call == {
+        "name": "KnowledgeBaseTool",
+        "thoughts": "这是一个检索的需求，我需要在KnowledgeBaseTool知识库中检索出与输入的query相关的段落，并返回给用户。",
+        "arguments": '{"query": "Hello, world!"}',
+    }
+    # FunctionMessage
+    assert response.chat_history[2].name == "KnowledgeBaseTool"
     assert (
-        response.chat_history[0].content
-        == "检索结果:\n\n    第1个段落: 住房和城乡建设部规章城市管理执法办法\n\n    第2个段落: 城市管理执法主管部门应当定期开展执法人员的培训和考核。\n\n"
-        "检索语句: Hello, world!\n请根据以上检索结果回答检索语句的问题"
+        response.chat_history[2].content
+        == '{"documents": [{"id": "495735246643269", "title": "城市管理执法办法.pdf", '
+        '"document": "住房和城乡建设部规章城市管理执法办法"}, {"id": "495735246643270", '
+        '"title": "城市管理执法办法.pdf", "document": "城市管理执法主管部门应当定期开展执法人员的培训和考核。"}]}'
     )
     # AIMessage
-    assert response.chat_history[1].content == "Text response"
-    assert response.chat_history[1].search_info == {
-        "results": [
-            {"index": 1, "url": "", "title": "城市管理执法办法.pdf"},
-            {"index": 2, "url": "", "title": "城市管理执法办法.pdf"},
+    assert response.chat_history[3].content == "Text response"
+
+    llm = FakeChatModelWithPresetResponses(
+        responses=[
+            AIMessage('{"is_relevant":false}', function_call=None),
+            AIMessage("Text response", function_call=None),
         ]
-    }
+    )
+    agent = FunctionalAgentWithRetrievalTool(
+        knowledge_base=search_db, llm=llm, tools=[identity_tool], memory=FakeMemory()
+    )
     # Test retrieval failed
     with mock.patch("requests.post") as my_mock:
-        my_mock.return_value = MagicMock(status_code=200, json=lambda: NO_EXAMPLE_RESPONSE)
+        my_mock.return_value = MagicMock(status_code=200, json=lambda: EXAMPLE_RESPONSE)
         response = await agent.async_run("Hello, world!")
-
     assert response.text == "Text response"
     # HumanMessage
     assert response.chat_history[0].content == "Hello, world!"
     # AIMessage
     assert response.chat_history[1].content == "Text response"
 
+ååå
 
 
 @pytest.mark.asyncio
-async def test_functional_agent_with_retrieval_run_tool(identity_tool, no_input_no_output_tool):
+async def test_functional_agent_with_retrieval_tool_run_tool(identity_tool, no_input_no_output_tool):
     knowledge_base_name = "test"
     access_token = "your access token"
     knowledge_base_id = 111
@@ -150,7 +169,7 @@ async def test_functional_agent_with_retrieval_run_tool(identity_tool, no_input_
         access_token=access_token,
         knowledge_base_id=knowledge_base_id if knowledge_base_id != "" else None,
     )
-    agent = FunctionalAgentWithRetrieval(
+    agent = FunctionalAgentWithRetrievalTool(
         knowledge_base=search_db,
         llm=FakeSimpleChatModel(),
         tools=[identity_tool, no_input_no_output_tool],
