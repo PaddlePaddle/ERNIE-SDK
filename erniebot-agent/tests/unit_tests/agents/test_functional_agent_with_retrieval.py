4ae51e4a402000db2ce21ba1c11b0ca0b2e97509
w5688414 <w5688414@gmail.com>
2023-12-25 08:15:24 +0000

    Add test_functional_agent_with_retrieval_run_retrieval

--- erniebot-agent/tests/unit_tests/agents/test_functional_agent_with_retrieval.py
+++ erniebot-agent/tests/unit_tests/agents/test_functional_agent_with_retrieval.py
@@ -3,10 +3,7 @@ from unittest import mock
 from unittest.mock import MagicMock
 
 import pytest
-from tests.unit_tests.agents.common_util import (
-    EXAMPLE_RESPONSE,
-    NO_EXAMPLE_RESPONSE,
-)
+from tests.unit_tests.agents.common_util import EXAMPLE_RESPONSE, NO_EXAMPLE_RESPONSE
 from tests.unit_tests.testing_utils.components import CountingCallbackHandler
 from tests.unit_tests.testing_utils.mocks.mock_chat_models import (
     FakeChatModelWithPresetResponses,
@@ -93,6 +90,53 @@ async def test_functional_agent_with_retrieval_callbacks(identity_tool):
         await agent.async_run("Hello, world!")
     assert callback_handler.run_starts == 1
     assert callback_handler.run_ends == 1
+
+
+@pytest.mark.asyncio
+async def test_functional_agent_with_retrieval_run_retrieval(identity_tool):
+    knowledge_base_name = "test"
+    access_token = "your access token"
+    knowledge_base_id = 111
+    with mock.patch("requests.post") as my_mock:
+        search_db = BaizhongSearch(
+            knowledge_base_name=knowledge_base_name,
+            access_token=access_token,
+            knowledge_base_id=knowledge_base_id if knowledge_base_id != "" else None,
+        )
+    agent = FunctionalAgentWithRetrieval(
+        knowledge_base=search_db, llm=FakeSimpleChatModel(), tools=[identity_tool], memory=FakeMemory()
+    )
+
+    # Test retrieval success
+    with mock.patch("requests.post") as my_mock:
+        my_mock.return_value = MagicMock(status_code=200, json=lambda: EXAMPLE_RESPONSE)
+        response = await agent.async_run("Hello, world!")
+
+    assert response.text == "Text response"
+    # HumanMessage
+    assert (
+        response.chat_history[0].content
+        == "检索结果:\n\n    第1个段落: 住房和城乡建设部规章城市管理执法办法\n\n    第2个段落: 城市管理执法主管部门应当定期开展执法人员的培训和考核。\n\n" \
+        "检索语句: Hello, world!\n请根据以上检索结果回答检索语句的问题"
+    )
+    # AIMessage
+    assert response.chat_history[1].content == "Text response"
+    assert response.chat_history[1].search_info == {
+        "results": [
+            {"index": 1, "url": "", "title": "城市管理执法办法.pdf"},
+            {"index": 2, "url": "", "title": "城市管理执法办法.pdf"},
+        ]
+    }
+    # Test retrieval failed
+    with mock.patch("requests.post") as my_mock:
+        my_mock.return_value = MagicMock(status_code=200, json=lambda: NO_EXAMPLE_RESPONSE)
+        response = await agent.async_run("Hello, world!")
+
+    assert response.text == "Text response"
+    # HumanMessage
+    assert response.chat_history[0].content == "Hello, world!"
+    # AIMessage
+    assert response.chat_history[1].content == "Text response"
 
 
 @pytest.mark.asyncio
