fff0ca7d6818be3188464ad3230088208bfd0370
Southpika <513923576@qq.com>
2023-12-27 10:41:12 +0800

    fix format

--- erniebot-agent/tests/unit_tests/tools/test_current_time.py
+++ erniebot-agent/tests/unit_tests/tools/test_current_time.py
@@ -9,13 +9,15 @@ from erniebot_agent.tools.current_time_tool import CurrentTimeTool
 def tool():
     return CurrentTimeTool()
 
+
 def test_schema(tool):
     function_call_schema = tool.function_call_schema()
-    assert (function_call_schema["description"] == CurrentTimeTool.description)
+    assert function_call_schema["description"] == CurrentTimeTool.description
     assert function_call_schema["responses"]["properties"]["current_time"]["type"] == "string"
+
 
 @pytest.mark.asyncio
 async def test_tool(tool):
     cur_time = await tool()
     print(cur_time)
-    assert len(cur_time['current_time']) > 6
+    assert len(cur_time["current_time"]) > 6
