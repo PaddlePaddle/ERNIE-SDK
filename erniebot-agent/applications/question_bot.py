317958261d214eef22ba43597ffecb84d1637998
Southpika <513923576@qq.com>
2024-01-04 10:38:56 +0800

    add code force

--- erniebot-agent/applications/question_bot.py
+++ erniebot-agent/applications/question_bot.py
@@ -22,9 +22,9 @@ from sklearn.metrics.pairwise import cosine_similarity
 from erniebot_agent.agents.function_agent_with_retrieval import (
     FunctionAgentWithRetrieval,
 )
-from erniebot_agent.chat_models.erniebot import ERNIEBot
+from erniebot_agent.chat_models import ERNIEBot
 from erniebot_agent.extensions.langchain.embeddings import ErnieEmbeddings
-from erniebot_agent.memory import SystemMessage
+from erniebot_agent.memory import SystemMessage, LimitTokensMemory
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--init", type=bool, default=False)
@@ -73,55 +73,23 @@ def open_and_concatenate_ipynb(ipynb_path, encoding):
     # 按顺序拼接Markdown文本和code单元
     concatenated_content = ""
     for cell in notebook_content["cells"]:
-        if cell["cell_type"] == "markdown":
-            concatenated_content += cell["source"] + "\n\n"
-        elif cell["cell_type"] == "code":
+        # if cell["cell_type"] == "markdown":
+        #     concatenated_content += cell["source"] + "\n\n"
+        if cell["cell_type"] == "code":
             concatenated_content += "```python\n" + cell["source"] + "```\n\n"
 
     # 返回拼接后的内容
     return concatenated_content
 
 
-class NotebookLoader(BaseLoader):
-    def __init__(
-        self,
-        file_path: str,
-        encoding: str = "utf-8",
-    ):
-        """Initialize with file path."""
-        self.file_path = file_path
-        self.encoding = encoding
-
-    def load(self) -> List[Document]:
-        """Load from file path."""
-        text = ""
-        try:
-            text = open_and_concatenate_ipynb(ipynb_path=self.file_path, encoding=self.encoding)
-        except UnicodeDecodeError as e:
-            if self.autodetect_encoding:
-                detected_encodings = detect_file_encodings(self.file_path)
-                for encoding in detected_encodings:
-                    try:
-                        with open(self.file_path, encoding=encoding.encoding) as f:
-                            text = f.read()
-                        break
-                    except UnicodeDecodeError:
-                        continue
-            else:
-                raise RuntimeError(f"Error loading {self.file_path}") from e
-        except Exception as e:
-            raise RuntimeError(f"Error loading {self.file_path}") from e
-
-        metadata = {"source": self.file_path}
-        return [Document(page_content=text, metadata=metadata)]
-
 
 class FaissSearch:
-    def __init__(self, db, embeddings):
+    def __init__(self, db, embeddings, module_db):
         self.db = db
+        self.module_db = module_db
         self.embeddings = embeddings
 
-    def search(self, query: str, top_k: int = 10, **kwargs):
+    def search(self, query: str, top_k: int = 2, **kwargs):
         docs = self.db.similarity_search(query, top_k)
         para_result = self.embeddings.embed_documents([i.page_content for i in docs])
         query_result = self.embeddings.embed_query(query)
@@ -140,13 +108,20 @@ class FaissSearch:
                 retrieval_results.append(
                     {"content": doc.page_content, "score": similarities[index], "title": ""}
                 )
+        code = self.module_db.similarity_search(query, 1)[0]
+        retrieval_results.append(
+            {"content": code.metadata['ipynb'], "score": 1, "title": code.page_content}
+        )
+        
         return retrieval_results
 
 
 def load_agent():
     faiss_name = "faiss_index"
+    faiss_name_module = "faiss_index_module"
     if not args.init:
         db = FAISS.load_local(faiss_name, embeddings)
+        module_db = FAISS.load_local(faiss_name_module, embeddings)
     else:
         md_file_path = [
             "./docs/modules/file.md",
@@ -154,6 +129,7 @@ def load_agent():
             "docs/modules/memory.md",
             "./docs/modules/message.md",
             "./docs/modules/chat_models.md",
+            "./docs/modules/tools.md",
         ]
         content = read_md_files(md_file_path)
         markdown_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
@@ -163,21 +139,28 @@ def load_agent():
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
         splits = text_splitter.split_documents(md_header_splits)
         db = FAISS.from_documents(splits, embeddings)
+        db.save_local(faiss_name)
 
         ipynb_path = [
             "./docs/cookbooks/agent/function_agent.ipynb",
             "./docs/cookbooks/agent/chat_models.ipynb",
             "./docs/cookbooks/agent/memory.ipynb",
             "./docs/cookbooks/agent/message.ipynb",
+            "./docs/cookbooks/agent/local_tool.ipynb",
+            "./docs/cookbooks/agent/tools_intro.ipynb",
         ]
-        for notebook in ipynb_path:
-            concatenated_content = open_and_concatenate_ipynb(notebook, encoding="utf-8")
-            text_splitter = CharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=100)
-            db.add_documents(text_splitter.create_documents([concatenated_content]))
-        db.save_local(faiss_name)
+        modules = [item[item.rfind('/')+1:item.rfind('.ipynb')] for item in ipynb_path]
+        module_doc = []
+        from langchain_core.documents import Document
+        for i in range(len(modules)):
+            module_doc.append(Document(page_content=modules[i], metadata={'ipynb': open_and_concatenate_ipynb(ipynb_path[i], 'utf-8')}))
+        
+        module_db = FAISS.from_documents(module_doc, embeddings)
+        module_db.save_local(faiss_name_module)
 
     llm = ERNIEBot(model="ernie-3.5")
-    faiss_search = FaissSearch(db=db, embeddings=embeddings)
+    faiss_search = FaissSearch(db=db, embeddings=embeddings, module_db=module_db)
+    memory = LimitTokensMemory(max_token_limit=4000)
     agent = FunctionAgentWithRetrieval(
         llm=llm,
         tools=[],
@@ -187,6 +170,9 @@ def load_agent():
             "你是ERNIEBot Agent的小助手，用于解决用户关于EB-Agent的问题，"
             "涉及File, Memory, Message, Agent, ChatModels等模块。请你严格按照搜索到的内容回答，不要自己生成相关代码。"
         ),
+        top_k=2,
+        token_limit=5000,
+        memory=memory
     )
     return agent
 
