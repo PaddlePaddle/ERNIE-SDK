bf085e5776fcc5280c0b195c516af9bcfea3b8d4
Southpika <513923576@qq.com>
2024-01-08 20:15:18 +0800

    fix run llm

--- erniebot-agent/src/erniebot_agent/agents/function_agent_with_retrieval.py
+++ erniebot-agent/src/erniebot_agent/agents/function_agent_with_retrieval.py
@@ -122,7 +122,6 @@ class FunctionAgentWithRetrieval(FunctionAgent):
                 llm_resp = await self._run_llm(
                     messages=chat_history,
                     functions=None,
-                    system=self.system.content if self.system is not None else None,
                 )
                 output_message = llm_resp.message
                 if output_message.search_info is None:
