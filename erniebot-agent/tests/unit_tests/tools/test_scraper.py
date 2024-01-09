c0802cf95101bc48434d3442331730e1b8bcad3f
wj-Mcat <1435130236@qq.com>
2024-01-09 13:52:46 +0800

    update scraper testing

--- erniebot-agent/tests/unit_tests/tools/test_scraper.py
+++ erniebot-agent/tests/unit_tests/tools/test_scraper.py
@@ -2,27 +2,34 @@ from __future__ import annotations
 
 import unittest
 
+from typing import List
 from erniebot_agent.tools.scraper import ScraperTool
 
 
 class TestScraper(unittest.IsolatedAsyncioTestCase):
-    async def test_run(self):
-        tool = ScraperTool("eb-agent")
 
-        content = await tool(
-            urls=[
-                {"url": "https://ernie-bot-agent.readthedocs.io/zh-cn/latest/"},
-                {"url": "https://arxiv.org/pdf/1810.04805.pdf"},
-                {"url": "https://arxiv.org/abs/2005.14165"},
-            ]
+    def setUp(self) -> None:
+        self.tool = ScraperTool("eb-agent")
+
+    async def _run_tool(self, url: str, keywords: List[str]):
+        content = await self.tool(
+            urls=[{"url": url}]
+        )
+        raw_content = content["result"][0]["raw_content"]
+        for keyword in keywords:
+            self.assertIn(keyword, raw_content)
+        
+    async def test_run(self):
+        await self._run_tool(
+            "https://ernie-bot-agent.readthedocs.io/zh-cn/latest/",
+            ["ERNIE Bot Agent", "ERNIE Bot"]
         )
 
-        result_0 = content["result"][0]["raw_content"]
-        self.assertIn("ERNIE Bot Agent", result_0)
-        self.assertIn("ERNIE Bot", result_0)
-
-        result_1 = content["result"][1]["raw_content"]
-        self.assertIn("BERT: Pre-training of Deep Bidirectional Transformers", result_1)
-
-        result_2 = content["result"][2]["raw_content"]
-        self.assertIn("GPT-3 in general.", result_2)
+        await self._run_tool(
+            "https://arxiv.org/pdf/1810.04805.pdf",
+            ["BERT: Pre-training of Deep Bidirectional Transformers"]
+        )
+        await self._run_tool(
+            "https://arxiv.org/abs/2005.14165",
+            ["GPT-3 in general."]
+        )
