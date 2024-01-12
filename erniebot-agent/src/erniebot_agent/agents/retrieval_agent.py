127eabe90f0dc91816d0664843698af213f4d1f2
w5688414 <w5688414@gmail.com>
2024-01-12 11:51:45 +0000

    Update retrieval agent

--- erniebot-agent/src/erniebot_agent/agents/retrieval_agent.py
+++ erniebot-agent/src/erniebot_agent/agents/retrieval_agent.py
@@ -7,24 +7,25 @@ from erniebot_agent.file import File
 from erniebot_agent.memory.messages import HumanMessage, Message
 from erniebot_agent.prompt import PromptTemplate
 
-GRAPH_PROMPT = """id: 查询的唯一id
+GRAPH_PROMPT = """请根据给定的query构建一个query子计算图。
+其中：id: 查询的唯一id
 dependencies: 在我们可以提出问题之前需要回答的子问题列表。当可能有未知的事情时，我们使用子查询，并且我们需要提出多个问题来得到答案。依赖项必须仅是其他查询。
 question: 我们正在使用问答系统提出的问题，如果我们提出多个问题，那么这个问题也是通过提供子问题的答案来提出的。
-example:
+示例:
 ##
 query: 美国和姚明的祖国的人口有什么区别？
-query_graph: {'query_graph': [{'dependencies': [],
-                    'id': 1,
-                    'question': "确定姚明的祖国"},
-                    {'dependencies': [],
-                    'id': 2,
-                    'question': '查找美国的人口'},
-                    {'dependencies': [1],
-                    'id': 3,
-                    'question': "查找姚明祖国的人口"},
-                    {'dependencies': [2, 3],
-                    'id': 4,
-                    'question': "分析加拿大和姚明祖国之间的人口差异"}]}
+query_graph: {"query_graph": [{"dependencies": [],
+      "id": 1,
+      "question": "确定姚明的祖国"},
+    {"dependencies": [],
+      "id": 2,
+      "question": "查找美国的人口"},
+    {"dependencies": [1],
+      "id": 3,
+      "question": "查找姚明祖国的人口"},
+    {"dependencies": [2, 3],
+      "id": 4,
+      "question": "分析加拿大和姚明祖国之间的人口差异"}]}
 ##
 query: {{query}}
 query_graph:
@@ -41,7 +42,15 @@ RAG_PROMPT = """
     第{{loop.index}}个段落: {{doc['content']}}
 {% endfor %}
 检索语句: {{query}}
-请根据以上检索结果回答检索语句的问题"""
+请根据以上检索结果回答检索语句的问题，如果不能回答，则回复{no_answer}"""
+
+FINAL_RAG_PROMPT = """
+检索结果:
+{% for doc in documents %}
+    第{{loop.index}}个子query: {{doc['query']}}, 搜索结果：{{doc['response']}}
+{% endfor %}
+检索语句: {{query}}
+请根据以上子query的搜索结果提供的信息回答检索语句的问题."""
 
 
 class DAGRetrievalAgent(Agent):
@@ -53,57 +62,70 @@ class DAGRetrievalAgent(Agent):
         **kwargs,
     ):
         super().__init__(**kwargs)
+        self.knowledge_base = knowledge_base
+        self.top_k = top_k
+        self.threshold = threshold
         self.query_transform = PromptTemplate(GRAPH_PROMPT, input_variables=["query"])
-        self.rag_prompt = PromptTemplate(RAG_PROMPT, input_variables=["documents", "query", "pre_context"])
+        self.sub_rag_prompt = PromptTemplate(
+            RAG_PROMPT, input_variables=["documents", "query", "pre_context"]
+        )
+        self.final_rag_prompt = PromptTemplate(FINAL_RAG_PROMPT, input_variables=["documents", "query"])
 
     async def _run(self, prompt: str, files: Optional[Sequence[File]] = None) -> AgentResponse:
         steps_taken: List[AgentStep] = []
 
         steps_input = HumanMessage(content=self.query_transform.format(query=prompt))
-        chat_history: List[Message] = [steps_input]
         # Query planning
         llm_resp = await self.run_llm(messages=[steps_input])
         output_message = llm_resp.message
         query_graph = self._parse_results(output_message.content)
-        retrieval_results = await self.execute(query_graph)
-
+        retrieval_results = await self.execute(query_graph, steps_taken)
         # Answer generation
-        step_input = HumanMessage(content=self.rag_prompt.format(query=prompt, documents=retrieval_results))
+        step_input = HumanMessage(
+            content=self.final_rag_prompt.format(query=prompt, documents=retrieval_results)
+        )
         chat_history: List[Message] = [step_input]
         llm_resp = await self.run_llm(
             messages=chat_history,
         )
-
-        chat_history.append(output_message)
+        response = llm_resp.message
+        chat_history.append(response)
         self.memory.add_message(chat_history[0])
         self.memory.add_message(chat_history[-1])
         response = self._create_finished_response(chat_history, steps_taken)
         return response
 
-    async def execute(self, query_graph: Dict):
+    async def execute(self, query_graph: Dict, steps_taken: List[AgentStep]):
         contexts = {}
-        for item in query_graph["query_graph"]:
+        for idx, item in enumerate(query_graph["query_graph"]):
             sub_query = item["question"]
             idx = item["id"]
             dependencies = item["dependencies"]
             # sub query search
             documents = await self.knowledge_base(sub_query, top_k=self.top_k, filters=None)
-
             # SubQuery Answer generation
             cur_contexts = []
             for dp in dependencies:
                 cur_context = contexts[dp]
                 cur_contexts.append(cur_context)
+            if len(cur_contexts) == 0:
+                cur_contexts = " "
             step_input = HumanMessage(
-                content=self.rag_prompt.format(query=sub_query, pre_context=cur_context, documents=documents)
+                content=self.sub_rag_prompt.format(
+                    query=sub_query, pre_context=cur_contexts, documents=documents["documents"]
+                )
             )
             chat_history: List[Message] = [step_input]
             llm_resp = await self.run_llm(
                 messages=chat_history,
             )
+            response = llm_resp.message
 
-            contexts[idx] = {"query": sub_query, "response": llm_resp.context}
-        return contexts.values()
+            contexts[idx] = {"query": sub_query, "response": response.content}
+            steps_taken.append(
+                AgentStep(info={"query": item, "name": f"sub query results {idx}"}, result=response.content)
+            )
+        return list(contexts.values())
 
     def _create_finished_response(
         self,
@@ -119,6 +141,7 @@ class DAGRetrievalAgent(Agent):
         )
 
     def _parse_results(self, results):
+        print(results)
         left_index = results.find("{")
         right_index = results.rfind("}")
         return json.loads(results[left_index : right_index + 1])
