2eba16b71d27d639816548b5d82bb357034cb643
wj-Mcat <1435130236@qq.com>
2024-01-19 11:57:22 +0800

    update file-testing

--- erniebot-agent/tests/unit_tests/tools/test_schema.py
+++ erniebot-agent/tests/unit_tests/tools/test_schema.py
@@ -34,7 +34,7 @@ from erniebot_agent.tools.schema import (
     is_optional_type,
     json_type,
 )
-from erniebot_agent.tools.utils import tool_response_contains_file
+from erniebot_agent.tools.utils import tool_response_contains_file, parse_json_request
 from erniebot_agent.utils.common import create_enum_class
 
 
@@ -503,6 +503,31 @@ class TestFileSchema(unittest.IsolatedAsyncioTestCase):
         file_content_from_file_manager = await file.read_contents()
         file_content = base64.b64decode(file_content_base64)
         self.assertEqual(file_content, file_content_from_file_manager)
+
+    @responses.activate
+    async def test_file_v8(self):
+        tool = self.toolkit.get_tool("file_v8")
+        file_manager = GlobalFileManagerHandler().get()
+
+        file_ids, file_contents = [], []
+        for _ in range(4):
+            file_content = str(uuid4())
+            file_content_base64 = base64.b64encode(file_content.encode())
+            file_contents.append(file_content.encode())
+
+            file = await file_manager.create_file_from_bytes(file_content_base64, filename="a.png")
+            file_ids.append(file.id)
+
+        responses.post(
+            "http://example.com/file_v8", json={}
+        )
+        self.assertIsNotNone(tool.tool_view.parameters)
+        tool_arguments = await parse_json_request(tool.tool_view.parameters, {"file": file_ids}, file_manager=file_manager)
+        for index, file_content in enumerate(tool_arguments["file"]):
+            self.assertEqual(file_contents[index], file_content)
+
+        result = await tool(file=file_ids)
+        self.assertEqual(len(result), 0)
 
 
 class TestEnumSchema(unittest.IsolatedAsyncioTestCase):
